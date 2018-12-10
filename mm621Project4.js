0402e352465eef67eca2cee5c16c51267c54fa26
Zachary Daly <zdaly@horizon.csueastbay.edu>
2018-12-10 10:40:50 -0800

    Add files via upload

--- mm621Project4.js
+++ mm621Project4.js
@@ -1,25 +1,27 @@
 var powerUseOnScreen;
-//var solarMonthlyCost;
-//var gridMonthlyCost;
-//var netDifferenceResult;
-//var monthList;
+var solarMonthlyCost;
+var gridMonthlyCost;
+var netDifferenceResult;
+var monthList;
+var monthSlider;
+var button;
 //var solarCostMessage;
 //var chickenPulling;
 //var zachPulling;
-//var allData;
+var allData;
+//var timeChange;
 
 
 //current month selected - will tie into createSlider() function
-//var monthSelector;
+var monthSelector;
 //var gridRate = 0.27;
-
-var monthSlider;
 
 function preload() {
   allData = loadJSON("data/allData.json");
-  chickenPulling = loadImage("data/chickenPulling.png");
-  zachPulling = loadImage("data/zachPulling.png");
+  //  chickenPulling = loadImage("data/chickenPulling.png");
+  //  zachPulling = loadImage("data/zachPulling.png");
   //load data on cost of power w/ grid only (difference of the above two will be the net result you need)
+  monthSlider = createSlider(0, 11, 6)
 }
 
 function setup() {
@@ -29,32 +31,48 @@ function setup() {
 
     createP("This is the total power use for the given month");
     //turn this into a running total counter
-    powerUseOnScreen = createP(allData.months[0].totalPowerUsage);
+    powerUseOnScreen == createP(allData.months[monthSelector].totalPowerUsage);
     //ladies and gentlemen we have a SUCCESSFUL callback function!!! BOO yaa!
   }
-    solarCost();
-    gridCost();
-    netResult();
-    currentMonth();
+  solarCost();
+  gridCost();
+  netResult();
+  currentMonth();
 
-//    createP("   ");
-//    createP("This is the test slider");
-//    monthSlider = createSlider(0, 11, 6);
-
-//    fill(255, 0, 175);
-  //  ellipse (100, 100, monthSlider.value, monthSlider.value);
+  //create slider that will serve as main user interface
+  button = createButton("GO");
+  createP("This is the test slider");
+  monthSlider;
 }
 
-//function draw() {
-  //background(100);
-  //imageLocations();
-//
+function draw() {
+  background(100);
+  fill(255, 0, 175);
+  //test circle to ensure code functions
+  ellipse(300, 300, monthSlider.value() * 20, monthSlider.value() * 20);
+  monthSelector == monthSlider.value();
+
+  //replace Y-location with variable connected to current month
+  //image(chickenPulling, 500, 500);
+  //image(zachPulling, 500, 500);
+
+//in theory, when the value on the slider (represented by monthSlider.value() ), increases or decreases, the monthSelector, which powers the array position selector (e.g. array[monthSelector]) will change causing all custom funcctions to "++" or "--" accordingly
+  if (monthSlider.value() ++) {
+    monthSelector++;
+  }
+
+  if (monthSlider.value() --) {
+    monthSelector--;
+  }
+
+}
+
 function solarCost() {
   if (solarMonthlyCost) {
 
     createP("This is the cost of solar power");
     //testing section & display numbers
-    solarMonthlyCost = createP(allData.months[0].solarCost);
+    solarMonthlyCost == createP(allData.months[monthSelector].solarCost);
     //solarCostMessage = join(solarMessageArray + seperator);
   }
 }
@@ -64,8 +82,7 @@ function gridCost() {
 
     createP("This is the cost of grid power");
     //testing section & display numbers
-    gridMonthlyCost = createP(allData.months[0].gridCost);
-    //solarCostMessage = join(solarMessageArray + seperator);
+    gridMonthlyCost == createP(allData.months[monthSelector].gridCost);
   }
 }
 
@@ -74,31 +91,17 @@ function netResult() {
 
     createP("This is the net difference of grid cost and solar cost");
     //testing section & display numbers
-    netDifferenceResult = createP(allData.months[0].netDifference);
+    netDifferenceResult == createP(allData.months[monthSelector].netDifference);
     //solarCostMessage = join(solarMessageArray + seperator);
   }
 }
 
-//function imageLocations() {
-  //replace Y-location with variable connected to current month
-  //image(chickenPulling, 500, 500);
-  //image(zachPulling, 500, 500);
-//}
-
 //slider controls what month
 function currentMonth() {
-  if(monthList){
+  if (monthList) {
 
-  createP("This is the current timeframe selected")
-  monthList = createP(allData.months[0].date);
-
-  //if (monthSelector++){
-    //solarCost, gridCost, and netDifference (all equipped with the changing month variable) all ++ to change their individual data types/view
-  //}
-
-  //if (monthSelector--){
-    //solarCost, gridCost, and netDifference (all equipped with the changing month variable) all -- to change their individual data types/view
-  //}
-
+    createP("This is the current timeframe selected")
+    monthList == createP(allData.months[monthSelector].date);
   }
+
 }

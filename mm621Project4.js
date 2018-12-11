623922bd6caecf22f47de52fdea528dd91d0b51f
Zachary Daly <zdaly@horizon.csueastbay.edu>
2018-12-10 17:02:13 -0800

    Add files via upload

--- mm621Project4.js
+++ mm621Project4.js
@@ -3,37 +3,44 @@ var button;
 //var solarCostMessage; (to display this quntity as a $ value in the on screen message)
 var tugOfWarImage;
 var allData;
-
-
 //current month selected - will tie into createSlider() function
 var monthSelector;
+//var netDifference;
+var input;
+var tugWarXPos;
 
 function preload() {
   allData = loadJSON("data/allData.json");
   tugOfWarImage = loadImage("data/tugOfWar.png");
   //load data on cost of power w/ grid only (difference of the above two will be the net result you need)
-
 }
 
 function setup() {
-  createCanvas(1080, 720);
-
-  //create slider that will serve as main user interface
+  createCanvas(1300, 740);
+  textSize(18);
+  fill(255, 240, 0);
+  textFont('Georgia');
   button = createButton("GO");
-  createP("This is the test slider");
-  monthSlider;
+  monthSlider = createSlider(0, 11, 6);
+  //input creation zone
+  input = createInput("Insert Data here");
 }
 
 function draw() {
   background(100);
-  //fill(255, 0, 175);
-  //test circle to ensure code functions. Wiull be replaced with monthSelector
-  //ellipse(200, 200, monthSlider.value() * 20, monthSlider.value() * 20);
-  monthSlider = createSlider(0, 11, 6);
+
+  //create slider that will serve as main user interface
+  // button = createButton("GO");
+  text(input.value(), 500, 200);
+  // monthSlider = createSlider(0, 11, 6);
   monthSelector = monthSlider.value();
+  monthSlider;
+  input;
+
 
   //tug-of-war image with X-location replaced by variable connected to current month
-  image(tugOfWarImage, monthSlider*20, 400, 700, 300);
+
+  image(tugOfWarImage, monthSelector*20+100, 90, 700, 300);
 
   solarCost();
   gridCost();
@@ -48,15 +55,16 @@ function draw() {
 function totalPowerUse(){
   if (allData.months[monthSelector].totalPowerUsage) {
 
-    createP("This is the total power use for the given month");
-
+    text("Power Usage (kWh)", 50, 500);
+    text(allData.months[monthSelector].totalPowerUsage, 75, 600);
   }
 }
 
 function solarCost() {
   if (allData.months[monthSelector].solarCost) {
 
-    createP("This is the cost of solar power");
+    text("Solar Cost (US$)", 230, 500);
+    text(allData.months[monthSelector].solarCost, 230, 600);
     //testing section & display numbers
     //solarCostMessage = join(solarMessageArray + seperator);
   }
@@ -65,7 +73,8 @@ function solarCost() {
 function gridCost() {
   if (allData.months[monthSelector].gridCost) {
 
-    createP("This is the cost of grid power");
+    text("Grid Cost (US$)", 400, 500);
+    text(allData.months[monthSelector].gridCost, 400, 600);
     //testing section & display numbers
   }
 }
@@ -73,16 +82,19 @@ function gridCost() {
 function netResult() {
   if (allData.months[monthSelector].netDifference) {
 
-    createP("This is the net difference of grid cost and solar cost");
+    text("Net Difference (US$)", 550, 500);
+    text(allData.months[monthSelector].netDifference, 550, 600);
     //testing section & display numbers
   }
+
 }
 
 //slider controls what month
 function currentMonth() {
   if (allData.months[monthSelector].date) {
 
-    createP("This is the current timeframe selected")
+    text("Timeframe", 750, 500);
+    text(allData.months[monthSelector].date, 750, 600);
 
   }
 
